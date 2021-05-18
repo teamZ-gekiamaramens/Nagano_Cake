9203265f1dbe11fb37b271485846618cd6e2b8b2
EC2 Default User <ec2-user@ip-172-31-55-233.ec2.internal>
2021-05-18 12:16:16 +0000

    index

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,2 +1,21 @@
 class Public::DeliveriesController < ApplicationController
+
+  def create
+    @deliverie = Deliverie.new
+    @deliverie = Deliverie.new(deliverie_params)
+    @deliverie.save
+    redirect_to public_deliveries_path
+  end
+
+  def index
+     @deliverie = Deliverie.new
+     @deliveries = Deliverie.all
+  end
+
+  private
+
+  def deliverie_params
+    params.require(:deliverie).permit(:name, :address, :postal_code)
+  end
+
 end
