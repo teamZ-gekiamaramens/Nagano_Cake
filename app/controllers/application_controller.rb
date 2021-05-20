332bc15cfe803ec31aefbde49a60040fb32e7166
tajimanaoto <tajima701025@gmail.com>
2021-05-20 09:23:14 +0000

    管理者ログイン機能追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    admin_customers_path # ログイン後に遷移するpathを設定
+  end
+
+  def after_sign_out_path_for(resource)
+    new_admin_session_path # ログアウト後に遷移するpathを設定
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+  sessions: 'admin/sessions'
+}
+
+  namespace :admin do
+    resources :customers, only: [:index,:show,:edit,:update]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
