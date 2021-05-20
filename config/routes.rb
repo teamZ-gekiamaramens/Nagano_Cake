915338459fe4785abf452535fbfeefbb7ccdd267
EC2 Default User <ec2-user@ip-172-31-28-165.us-east-2.compute.internal>
2021-05-20 08:50:19 +0000

    Customer/login/show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :customers
+   namespace :public do
+   resources :customers, only: [:show,:update,:index] do
+       collection do
+        get :edit
+        get 'unsubscribe'
+        patch "withdrawal"
+       end
+   end
+ end
 end
