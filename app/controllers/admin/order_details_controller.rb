aa9164bd4deaa67ee7f9ad0c8d31af31bd7c0190
tajimanaoto <tajima701025@gmail.com>
2021-05-24 10:43:16 +0000

    enum

---
+++ app/controllers/admin/order_details_controller.rb
@@ -0,0 +1,13 @@
+class Admin::OrderDetailsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(order_detail_params)
+    redirect_back(fallback_location: root_path)
+  end
+  private
+  def order_detail_params
+   params.require(:order_detail).permit(:making_status)
+  end
+end
--- app/models/order.rb
+++ app/models/order.rb
@@ -7,11 +7,5 @@ has_many :order_details, dependent: :destroy
     銀行振込: 1
   }
 
-  enum order_status: {
-    入金待ち: 0,
-    入金確認: 1,
-    製作中: 2,
-    発送準備中: 3,
-    発送済: 4
-  }
+  enum order_status: {pending: 0, confirm: 1, processing: 2, preparing: 3, shipped: 4}
 end
