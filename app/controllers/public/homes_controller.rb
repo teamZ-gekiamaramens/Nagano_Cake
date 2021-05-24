936902748ca16edcea32a375395a0e68c38b67c9
EC2 Default User <ec2-user@ip-172-31-18-70.ec2.internal>
2021-05-24 07:35:12 +0000

    商品一覧の修正と管理者&顧客の注文一覧追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,9 @@
 class Public::HomesController < ApplicationController
 
   def top
+    #ページング機能の追加=========================
+    @items = Item.page(params[:page]).per(4).reverse_order
+    #=============================================
   end
 
   def about
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
 
     #従属しているモデル====================
-    has_many :cart_items
+    has_many :cart_items , dependent: :destroy
     belongs_to :genre, optional: true
     #======================================
 
@@ -18,7 +18,9 @@ class Item < ApplicationRecord
     validates :image, presence: true
     validates :introduction, presence: true
     validates :price, presence: true
-    validates :is_active, presence: true
+    validates :is_active, inclusion:{in: [true, false]}
+    # boolean型は「presence: true」と定義すると、false =「nil」と認識されるので
+    #「inclusion:{in:[true,false]}で「true」と「false」の両方を認識させる 
     #======================================
 
 end
