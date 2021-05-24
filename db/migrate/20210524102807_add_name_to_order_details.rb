90a7479709e24d515c460be13bc4c975ff840ca9
EC2 Default User <ec2-user@ip-172-31-28-165.us-east-2.compute.internal>
2021-05-24 10:31:34 +0000

    order_detail

---
+++ db/migrate/20210524102807_add_name_to_order_details.rb
@@ -0,0 +1,8 @@
+class AddNameToOrderDetails < ActiveRecord::Migration[5.2]
+  def change
+    add_column :order_details, :item_id, :integert
+    add_column :order_details, :quantity, :integer
+    add_column :order_details, :price, :integer
+    add_column :order_details, :create_status, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_24_095606) do
+ActiveRecord::Schema.define(version: 2021_05_24_102807) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -82,6 +82,10 @@ ActiveRecord::Schema.define(version: 2021_05_24_095606) do
   create_table "order_details", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "item_id"
+    t.integer "quantity"
+    t.integer "price"
+    t.integer "create_status"
   end
 
   create_table "orders", force: :cascade do |t|
