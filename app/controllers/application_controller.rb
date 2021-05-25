c9ad81b59bc094e15e5d847fcb39e27e9c4265a0
EC2 Default User <ec2-user@ip-172-31-28-165.us-east-2.compute.internal>
2021-05-25 09:15:41 +0000

    log_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,12 @@ end
 
 
 
- def after_sign_out_path_for(resource)
-   root_path
+ def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
  end
 
 
